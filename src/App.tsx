a2b3f919b80f477c2825983d6abcbc0f1e624b24
Ido Schacham <idosius@gmail.com>
2025-04-07 13:19:21 +0200

    feat: Basic text editing interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './App.module.css'
+import { ChangeEvent } from 'react'
+import CutUpLine from './components/CutUpLine.tsx'
+import { useLocalStorage } from '@uidotdev/usehooks'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useLocalStorage('text', '')
+  const cutUpText = text.split('\n').filter((line) => line.trim().length > 0)
+
+  function handleOnChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    setText(event.target.value)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className={styles.root}>
+      <textarea rows={40} cols={50} onChange={handleOnChange}>
+        {text}
+      </textarea>
+      <div className={styles.lines}>
+        {cutUpText.map((text) => (
+          <CutUpLine key={text} text={text} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
---
+++ src/components/CutUpLine.tsx
@@ -0,0 +1,7 @@
+type CutUpLineProps = {
+  text: string
+}
+
+export default function CutUpLine({text}: CutUpLineProps) {
+  return <p>{text}</p>
+}
